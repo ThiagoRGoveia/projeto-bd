b37a1d479316c6203a22bff36f5d40b1f2698d85
uriel <urielcuelho@hotmail.com>
2021-11-22 12:01:46 -0300

    inserts

---
+++ backend/src/dml/inserts.sql
@@ -0,0 +1,235 @@
+--(id individuo, nome)
+INSERT INTO individuos VALUES(1,'Jairo Bolzonaro');
+INSERT INTO individuos VALUES(2,'Luiz Polvo');
+INSERT INTO individuos VALUES(3,'Cesar Nogueira');
+INSERT INTO individuos VALUES(4,'Charles Lupin');
+INSERT INTO individuos VALUES(5,'Glenda Hoff');
+INSERT INTO individuos VALUES(6,'Jefferson Robert');
+INSERT INTO individuos VALUES(7,'Whale Rossi');
+INSERT INTO individuos VALUES(8,'Joseph Mary');
+INSERT INTO individuos VALUES(9,'Lucian Twovar');
+INSERT INTO individuos VALUES(10,'Helen Hellouise');
+INSERT INTO individuos VALUES(11,'Renatta Hellmeister');
+INSERT INTO individuos VALUES(12,'Shepherd Everald');
+INSERT INTO individuos VALUES(13,'Edmilson Coast');
+INSERT INTO individuos VALUES(14,'Valdemar Coast Grandson');
+INSERT INTO individuos VALUES(15,'Antonio Fisher Grandson');
+INSERT INTO individuos VALUES(16,'Joseph Mary Eymael');
+INSERT INTO individuos VALUES(17,'Aldinea Fidélica');
+INSERT INTO individuos VALUES(18,'Rui Coast Pepper');
+INSERT INTO individuos VALUES(19,'Eduardo Riacho');
+INSERT INTO individuos VALUES(20,'Antonio Macarone');
+INSERT INTO individuos VALUES(21,'Suze Haidar');
+INSERT INTO individuos VALUES(22,'Daniel Chifre');
+INSERT INTO individuos VALUES(23,'Carlos Sikera');
+INSERT INTO individuos VALUES(24,'Jose Luiz Clemencia');
+INSERT INTO individuos VALUES(25,'Bruno Cavaco Araujes');
+INSERT INTO individuos VALUES(26,'Julio Medeiros');
+INSERT INTO individuos VALUES(27,'Giu Kassab');
+INSERT INTO individuos VALUES(28,'Luciana Anjos');
+INSERT INTO individuos VALUES(29,'Guilherme Cafe');
+INSERT INTO individuos VALUES(30,'John Amoedo');
+INSERT INTO individuos VALUES(31,'Sergio Morno');
+INSERT INTO individuos VALUES(32,'Carlos Gomes');
+INSERT INTO individuos VALUES(33,'Mariana Silva');
+INSERT INTO individuos VALUES(34,'Michel Bemer');
+INSERT INTO individuos VALUES(35,'Selma Soullef');
+INSERT INTO individuos VALUES(36,'Quindin Kataguiri');
+INSERT INTO individuos VALUES(37,'Arthur Durval');
+INSERT INTO individuos VALUES(38,'John Doria');
+INSERT INTO individuos VALUES(39,'Elizabeth Penna');
+INSERT INTO individuos VALUES(40,'Tabata Loveral');
+
+
+
+--(id partido , nome, sigla, numero eleitoral, programa)
+INSERT INTO partidos VALUES(11,'Progressistas','PP',11,'');
+INSERT INTO partidos VALUES(12,'Partido Democratico Trabalhista','PDT',12,'');
+INSERT INTO partidos VALUES(13,'Partido dos Trabalhadores','PT',13,'');
+INSERT INTO partidos VALUES(14,'Partido Trabalhista Brasileiro','PTB',14,'');
+INSERT INTO partidos VALUES(15,'Movimento Democrático Brasileiro','MDB',15,'');
+INSERT INTO partidos VALUES(16,'Partido Socialista dos Trabalhadores Unificado','PSTU',16,'');
+INSERT INTO partidos VALUES(17,'Partido Social Liberal','PSL',17,'');
+INSERT INTO partidos VALUES(18,'Rede Sustentabilidade','REDE',18,'');
+INSERT INTO partidos VALUES(19,'Podemos','PODE',19,'');
+INSERT INTO partidos VALUES(20,'Partido Social Cristão','PSC',20,'');
+INSERT INTO partidos VALUES(21,'Partido Comunista Brasileiro','PCB',21,'');
+INSERT INTO partidos VALUES(22,'Partido Liberal','PL',22,'');
+INSERT INTO partidos VALUES(25,'Democratas','DEM',25,'');
+INSERT INTO partidos VALUES(27,'Democracia crista','DC',27,'');
+INSERT INTO partidos VALUES(28,'Partido Renovador Trabalhista Brasileiro','PRTB',28,'');
+INSERT INTO partidos VALUES(29,'Partido da Causa Operaria','PCO',29,'');
+INSERT INTO partidos VALUES(30,'Partido Novo','NOVO',30,'');
+INSERT INTO partidos VALUES(33,'Partido da Mobilização Nacional','PMN',33,'');
+INSERT INTO partidos VALUES(35,'Partido da Mulher Brasileira','PMB',35,'');
+INSERT INTO partidos VALUES(36,'Partido Trabalhista Cristão','PTC',36,'');
+INSERT INTO partidos VALUES(40,'Partido Socialista Brasileiro','PSB',40,'');
+INSERT INTO partidos VALUES(43,'Partido Verde','PV',43,'');
+INSERT INTO partidos VALUES(45,'Partido da Social Democracia Brasileira','PSDB',45,'');
+INSERT INTO partidos VALUES(50,'Partido Socialismo e Liberdade','PSOL',50,'');
+INSERT INTO partidos VALUES(55,'Partido Social Democrático','PSD',55,'');
+INSERT INTO partidos VALUES(65,'Partido Comunista do Brasil','PCdoB',65,'');
+
+
+--(id pessoa fisica, cpf, data de nascimento, id individuo)
+INSERT INTO individuos_pessoa_fisica VALUES(1,'36438079022','1951-05-30',1);
+INSERT INTO individuos_pessoa_fisica VALUES(2,'89237314043','1956-08-12',2);
+INSERT INTO individuos_pessoa_fisica VALUES(3,'79242041050','1981-08-26',3);
+INSERT INTO individuos_pessoa_fisica VALUES(4,'24918125093','1974-06-25',4);
+INSERT INTO individuos_pessoa_fisica VALUES(5,'84506179013','1981-03-24',5);
+INSERT INTO individuos_pessoa_fisica VALUES(6,'83294655002','1968-02-19',6);
+INSERT INTO individuos_pessoa_fisica VALUES(7,'16589072051','1968-04-11',7);
+INSERT INTO individuos_pessoa_fisica VALUES(8,'96189150004','1974-04-16',8);
+INSERT INTO individuos_pessoa_fisica VALUES(9,'81879608090','1973-05-28',9);
+INSERT INTO individuos_pessoa_fisica VALUES(10,'81102616044','1985-07-03',10);
+INSERT INTO individuos_pessoa_fisica VALUES(11,'32100819054','1971-06-24',11);
+INSERT INTO individuos_pessoa_fisica VALUES(12,'39175207028','1972-06-09',12);
+INSERT INTO individuos_pessoa_fisica VALUES(13,'94322544029','1975-12-06',13);
+INSERT INTO individuos_pessoa_fisica VALUES(14,'72738437095','1981-05-24',14);
+INSERT INTO individuos_pessoa_fisica VALUES(15,'06885694045','1969-10-01',15);
+INSERT INTO individuos_pessoa_fisica VALUES(16,'43047588023','1976-06-14',16);
+INSERT INTO individuos_pessoa_fisica VALUES(17,'96551509096','1974-03-22',17);
+INSERT INTO individuos_pessoa_fisica VALUES(18,'75193424090','1977-08-28',18);
+INSERT INTO individuos_pessoa_fisica VALUES(19,'17394404048','1976-06-10',19);
+INSERT INTO individuos_pessoa_fisica VALUES(20,'55666799002','1988-12-14',20);
+INSERT INTO individuos_pessoa_fisica VALUES(21,'20722464070','1984-05-12',21);
+INSERT INTO individuos_pessoa_fisica VALUES(22,'36041473014','1974-12-08',22);
+INSERT INTO individuos_pessoa_fisica VALUES(23,'04696561070','1978-10-07',23);
+INSERT INTO individuos_pessoa_fisica VALUES(24,'66119933042','1995-12-15',24);
+INSERT INTO individuos_pessoa_fisica VALUES(25,'69946153084','1978-09-25',25);
+INSERT INTO individuos_pessoa_fisica VALUES(26,'07752917060','1974-04-20',26);
+INSERT INTO individuos_pessoa_fisica VALUES(27,'87094546084','1987-08-07',27);
+INSERT INTO individuos_pessoa_fisica VALUES(28,'64867662046','1995-01-18',28);
+INSERT INTO individuos_pessoa_fisica VALUES(29,'49079935018','1977-09-10',29);
+INSERT INTO individuos_pessoa_fisica VALUES(30,'11166086003','1982-03-18',30);
+INSERT INTO individuos_pessoa_fisica VALUES(31,'83936637091','1990-10-03',31);
+INSERT INTO individuos_pessoa_fisica VALUES(32,'66867569030','1992-12-06',32);
+INSERT INTO individuos_pessoa_fisica VALUES(33,'81426922019','1968-12-26',33);
+INSERT INTO individuos_pessoa_fisica VALUES(34,'66583601036','1974-10-18',34);
+INSERT INTO individuos_pessoa_fisica VALUES(35,'43735929001','1980-04-23',35);
+INSERT INTO individuos_pessoa_fisica VALUES(36,'93100886054','1981-03-15',36);
+INSERT INTO individuos_pessoa_fisica VALUES(37,'89006728004','1968-11-11',37);
+INSERT INTO individuos_pessoa_fisica VALUES(38,'93759728014','1993-06-14',38);
+INSERT INTO individuos_pessoa_fisica VALUES(39,'07074872008','1989-10-05',39);
+INSERT INTO individuos_pessoa_fisica VALUES(40,'38011962086','1987-03-10',40);
+
+--(id cadidato, id pessoa fisica, id partido)
+INSERT INTO candidatos VALUES(1,1,17);
+INSERT INTO candidatos VALUES(2,2,13);
+INSERT INTO candidatos VALUES(3,3,11);
+INSERT INTO candidatos VALUES(4,4,12);
+INSERT INTO candidatos VALUES(5,5,13);
+INSERT INTO candidatos VALUES(6,6,14);
+INSERT INTO candidatos VALUES(7,7,15);
+INSERT INTO candidatos VALUES(8,8,16);
+INSERT INTO candidatos VALUES(9,9,17);
+INSERT INTO candidatos VALUES(10,10,18);
+INSERT INTO candidatos VALUES(11,11,19);
+INSERT INTO candidatos VALUES(12,12,20);
+INSERT INTO candidatos VALUES(13,13,21);
+INSERT INTO candidatos VALUES(14,14,22);
+INSERT INTO candidatos VALUES(15,15,25);
+INSERT INTO candidatos VALUES(16,16,27);
+INSERT INTO candidatos VALUES(17,17,28);
+INSERT INTO candidatos VALUES(18,18,29);
+INSERT INTO candidatos VALUES(19,19,30);
+INSERT INTO candidatos VALUES(20,20,33);
+INSERT INTO candidatos VALUES(21,21,35);
+INSERT INTO candidatos VALUES(22,22,36);
+INSERT INTO candidatos VALUES(23,23,40);
+INSERT INTO candidatos VALUES(24,24,43);
+INSERT INTO candidatos VALUES(25,25,45);
+INSERT INTO candidatos VALUES(26,26,50);
+INSERT INTO candidatos VALUES(27,27,55);
+INSERT INTO candidatos VALUES(28,28,65);
+INSERT INTO candidatos VALUES(29,29,50);
+INSERT INTO candidatos VALUES(30,30,30);
+INSERT INTO candidatos VALUES(31,31,19);
+INSERT INTO candidatos VALUES(32,32,12);
+INSERT INTO candidatos VALUES(33,33,18);
+INSERT INTO candidatos VALUES(34,34,15);
+INSERT INTO candidatos VALUES(35,35,13);
+INSERT INTO candidatos VALUES(36,36,25);
+INSERT INTO candidatos VALUES(37,37,25);
+INSERT INTO candidatos VALUES(38,38,45);
+INSERT INTO candidatos VALUES(39,39,50);
+INSERT INTO candidatos VALUES(40,40,12);
+
+
+INSERT INTO cargos VALUES(01,'Presidente da Republica',1,NULL,NULL,TRUE);
+INSERT INTO cargos VALUES(02,'Deputado Federal',513,NULL,NULL,TRUE);
+INSERT INTO cargos VALUES(03,'Senador',81,NULL,NULL,TRUE);
+
+INSERT INTO cargos VALUES(101,'Governador',1,NULL,'Acre',FALSE);
+INSERT INTO cargos VALUES(102,'Governador',1,NULL,'Alagoas',FALSE);
+INSERT INTO cargos VALUES(103,'Governador',1,NULL,'Amapa',FALSE);
+INSERT INTO cargos VALUES(104,'Governador',1,NULL,'Amazonas',FALSE);
+INSERT INTO cargos VALUES(105,'Governador',1,NULL,'Bahia',FALSE);
+INSERT INTO cargos VALUES(106,'Governador',1,NULL,'Ceara',FALSE);
+INSERT INTO cargos VALUES(107,'Governador',1,NULL,'Espirito Santo',FALSE);
+INSERT INTO cargos VALUES(108,'Governador',1,NULL,'Goias',FALSE);
+INSERT INTO cargos VALUES(109,'Governador',1,NULL,'Maranhao',FALSE);
+INSERT INTO cargos VALUES(110,'Governador',1,NULL,'Mato Grosso',FALSE);
+INSERT INTO cargos VALUES(111,'Governador',1,NULL,'Mato Grosso do Sul',FALSE);
+INSERT INTO cargos VALUES(112,'Governador',1,NULL,'Minas Gerais',FALSE);
+INSERT INTO cargos VALUES(113,'Governador',1,NULL,'Para',FALSE);
+INSERT INTO cargos VALUES(114,'Governador',1,NULL,'Paraiba',FALSE);
+INSERT INTO cargos VALUES(115,'Governador',1,NULL,'Parana',FALSE);
+INSERT INTO cargos VALUES(116,'Governador',1,NULL,'Pernambuco',FALSE);
+INSERT INTO cargos VALUES(117,'Governador',1,NULL,'Piaui',FALSE);
+INSERT INTO cargos VALUES(118,'Governador',1,NULL,'Rio de Janeiro',FALSE);
+INSERT INTO cargos VALUES(119,'Governador',1,NULL,'Rio Grande do Norte',FALSE);
+INSERT INTO cargos VALUES(120,'Governador',1,NULL,'Rio Grande do Sul',FALSE);
+INSERT INTO cargos VALUES(121,'Governador',1,NULL,'Rondonia',FALSE);
+INSERT INTO cargos VALUES(122,'Governador',1,NULL,'Roraima',FALSE);
+INSERT INTO cargos VALUES(123,'Governador',1,NULL,'Santa Catarina',FALSE);
+INSERT INTO cargos VALUES(124,'Governador',1,NULL,'Sao Paulo',FALSE);
+INSERT INTO cargos VALUES(125,'Governador',1,NULL,'Sergipe',FALSE);
+INSERT INTO cargos VALUES(126,'Governador',1,NULL,'Tocantins',FALSE);
+
+INSERT INTO cargos VALUES(201,'Deputado Estadual',24,NULL,'Acre',FALSE);
+INSERT INTO cargos VALUES(202,'Deputado Estadual',27,NULL,'Alagoas',FALSE);
+INSERT INTO cargos VALUES(203,'Deputado Estadual',24,NULL,'Amapa',FALSE);
+INSERT INTO cargos VALUES(204,'Deputado Estadual',24,NULL,'Amazonas',FALSE);
+INSERT INTO cargos VALUES(205,'Deputado Estadual',63,NULL,'Bahia',FALSE);
+INSERT INTO cargos VALUES(206,'Deputado Estadual',46,NULL,'Ceara',FALSE);
+INSERT INTO cargos VALUES(207,'Deputado Estadual',30,NULL,'Espirito Santo',FALSE);
+INSERT INTO cargos VALUES(208,'Deputado Estadual',41,NULL,'Goias',FALSE);
+INSERT INTO cargos VALUES(209,'Deputado Estadual',42,NULL,'Maranhao',FALSE);
+INSERT INTO cargos VALUES(210,'Deputado Estadual',24,NULL,'Mato Grosso',FALSE);
+INSERT INTO cargos VALUES(211,'Deputado Estadual',24,NULL,'Mato Grosso do Sul',FALSE);
+INSERT INTO cargos VALUES(212,'Deputado Estadual',77,NULL,'Minas Gerais',FALSE);
+INSERT INTO cargos VALUES(213,'Deputado Estadual',41,NULL,'Para',FALSE);
+INSERT INTO cargos VALUES(214,'Deputado Estadual',36,NULL,'Paraiba',FALSE);
+INSERT INTO cargos VALUES(215,'Deputado Estadual',54,NULL,'Parana',FALSE);
+INSERT INTO cargos VALUES(216,'Deputado Estadual',49,NULL,'Pernambuco',FALSE);
+INSERT INTO cargos VALUES(217,'Deputado Estadual',30,NULL,'Piaui',FALSE);
+INSERT INTO cargos VALUES(218,'Deputado Estadual',70,NULL,'Rio de Janeiro',FALSE);
+INSERT INTO cargos VALUES(219,'Deputado Estadual',24,NULL,'Rio Grande do Norte',FALSE);
+INSERT INTO cargos VALUES(220,'Deputado Estadual',55,NULL,'Rio Grande do Sul',FALSE);
+INSERT INTO cargos VALUES(221,'Deputado Estadual',24,NULL,'Rondonia',FALSE);
+INSERT INTO cargos VALUES(222,'Deputado Estadual',24,NULL,'Roraima',FALSE);
+INSERT INTO cargos VALUES(223,'Deputado Estadual',40,NULL,'Santa Catarina',FALSE);
+INSERT INTO cargos VALUES(224,'Deputado Estadual',94,NULL,'Sao Paulo',FALSE);
+INSERT INTO cargos VALUES(225,'Deputado Estadual',24,NULL,'Sergipe',FALSE);
+INSERT INTO cargos VALUES(226,'Deputado Estadual',24,NULL,'Tocantins',FALSE);
+
+INSERT INTO cargos VALUES(301,'Prefeito',1,'Sao Carlos',NULL,FALSE);
+INSERT INTO cargos VALUES(302,'Verador',21,'Sao Carlos',NULL,FALSE);
+
+INSERT INTO pleitos VALUES();
+
+
+INSERT INTO candidaturas VALUES();
+
+
+INSERT INTO processos_judiciais VALUES();
+
+
+INSERT INTO equipes_apoio VALUES();
+
+
+INSERT INTO candidatura_doacao VALUES();
+
+
+INSERT INTO individuos_pessoa_juridica VALUES();
